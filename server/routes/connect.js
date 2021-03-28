c0f38ec51e4f831f85ee3104bfd180ef0cbeea3b
N00bAdrian <adrianwong227@gmail.com>
2021-03-28 12:48:24 +0100

    update

--- server/routes/connect.js
+++ server/routes/connect.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('../database.db', (err) {
+  if (err) throw err;
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('connect', { title: 'Connect' });
+  db.all(`SELECT username, preferences FROM users`, [], (err, rows) => {
+    if (err) throw err;
+    res.render('connect', { 
+      title: 'Connect',
+      rows: rows
+    });
+  });
 });
 
 
